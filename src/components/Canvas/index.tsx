5b2a1ba4c901309aaf9f79f7c44e4831c4b3a4ff
kyamashiro <kyamashiro73@gmail.com>
2024-04-15 19:23:25 +0900

    WIP

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -1,16 +1,14 @@
 import React, { useRef, useState } from "react";
 import * as d3 from "d3";
 
-interface MousePosition {
-  x: number;
-  y: number;
-}
+export type Points = [number, number][];
 
 export const Canvas: React.FC = () => {
   const height = 2900;
   const svgRef = useRef<SVGSVGElement>(null);
   const [isMouseDown, setIsMouseDown] = useState(false);
-  const points: MousePosition[] = [];
+  const [points, setPoints] = useState<Points>([]);
+
   // @ts-ignore
   const [currentPath, setCurrentPath] = useState<Selection<
     SVGSVGElement | null,
@@ -20,55 +18,34 @@ export const Canvas: React.FC = () => {
   > | null>(null);
   const color = "rgba(0, 0, 0, 1)";
 
-  const startDrawing = () => {
+  const startDrawing = (e: React.MouseEvent<SVGSVGElement>) => {
     setIsMouseDown(true);
+    if (!svgRef.current) return;
+    const [x, y] = d3.pointer(e);
+    setPoints((prev) => [...prev, [x, y]]);
+    setCurrentPath(
+      d3
+        .select<SVGSVGElement, unknown>(svgRef.current)
+        .append("path")
+        .style("stroke-width", 10)
+        .style("fill", "none")
+        .attr("stroke", color)
+        .style("stroke-linecap", "round")
+    );
   };
 
   const endDrawing = () => {
     setIsMouseDown(false);
+    setCurrentPath(null);
+    setPoints([]);
   };
-
-  const getMousePosition = (
-    svg: SVGSVGElement,
-    e: React.MouseEvent<SVGSVGElement>
-  ) => {
-    const rect = svg.getBoundingClientRect();
-
-    return {
-      x: ((e.clientX - rect.left) / (rect.right - rect.left)) * svg.clientWidth,
-      y: ((e.clientY - rect.top) / (rect.bottom - rect.top)) * svg.clientHeight,
-    };
-  };
-
-  const [transformedObj, setTransformedObj] = useState<any>([]);
 
   const drawing = (e: React.MouseEvent<SVGSVGElement>) => {
     if (!isMouseDown) return;
     if (!svgRef.current) return;
-    const { x, y } = getMousePosition(svgRef.current, e);
-    points.push({ x, y });
-
-    if (!currentPath) {
-      setCurrentPath(
-        d3
-          .select<SVGSVGElement, unknown>(svgRef.current)
-          .append("path")
-          .style("stroke-width", 10)
-          .style("fill", "none")
-          .attr("stroke", color)
-          .style("stroke-linecap", "round")
-      );
-    }
-    // currentPath.datum(points).attr("d", d3.line().curve(d3.curveBasis));
-
-    let path = `M${points[0].x},${points[0].y}`;
-
-    for (let i = 1; i < points.length; i++) {
-      const { x, y } = points[i];
-      path += `L${x},${y}`;
-    }
-
-    setTransformedObj([...transformedObj, path]);
+    const [x, y] = d3.pointer(e);
+    setPoints((prev) => [...prev, [x, y]]);
+    currentPath.datum(points).attr("d", d3.line().curve(d3.curveBasis));
   };
 
   return (
@@ -77,23 +54,10 @@ export const Canvas: React.FC = () => {
         ref={svgRef}
         width={1000}
         height={height}
-        onMouseDown={() => startDrawing()}
+        onMouseDown={(e) => startDrawing(e)}
         onMouseUp={() => endDrawing()}
         onMouseMove={(e) => drawing(e)}
-      >
-        {transformedObj.map((d: any, i: any) => {
-          return (
-            <path
-              key={i}
-              stroke={color}
-              strokeWidth={10}
-              fill="none"
-              strokeLinecap="round"
-              d={d}
-            />
-          );
-        })}
-      </svg>
+      />
     </div>
   );
 };
